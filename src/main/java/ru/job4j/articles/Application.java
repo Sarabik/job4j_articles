6a7e5057de12860671f4eae9652c397fe765b9c4
Sarabik <dalilija@inbox.lv>
2023-01-02 12:35:49 +0100

    2. GC. Out Of Mem. Тестовое задание. [#504790]

--- src/main/java/ru/job4j/articles/Application.java
+++ src/main/java/ru/job4j/articles/Application.java
@@ -16,13 +16,15 @@ public class Application {
 
     public static final int TARGET_COUNT = 1_000_000;
 
+    public static final int CACHE_ARTICLES = 25_000;
+
     public static void main(String[] args) {
         var properties = loadProperties();
         var wordStore = new WordStore(properties);
         var articleStore = new ArticleStore(properties);
         var articleGenerator = new RandomArticleGenerator();
         var articleService = new SimpleArticleService(articleGenerator);
-        articleService.generate(wordStore, TARGET_COUNT, articleStore);
+        articleService.generate(wordStore, TARGET_COUNT, articleStore, CACHE_ARTICLES);
     }
 
     private static Properties loadProperties() {
